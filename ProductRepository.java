c92f65a6ecbc7597fb64bf0031f1063235c987b5
Lucas Kraglievich <lucaskra2001@gmail.com>
2026-07-20 19:07:03 -0300

    Delete ProductRepository.java

--- ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.day22.repository;
-
-import com.example.day22.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}

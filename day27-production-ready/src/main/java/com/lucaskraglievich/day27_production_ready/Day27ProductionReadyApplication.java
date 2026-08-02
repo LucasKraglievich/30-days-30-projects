088624f34dbf26defe5a97377687a924f4ef4cd6
lucaskraglievich <lucaskra2001@gmail.com>
2026-08-02 18:53:59 -0300

    Día 27: Production-readiness con Actuator, Profiles y JAR ejecutable

---
+++ day27-production-ready/src/main/java/com/lucaskraglievich/day27_production_ready/Day27ProductionReadyApplication.java
@@ -0,0 +1,13 @@
+package com.lucaskraglievich.day27_production_ready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day27ProductionReadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day27ProductionReadyApplication.class, args);
+	}
+
+}
